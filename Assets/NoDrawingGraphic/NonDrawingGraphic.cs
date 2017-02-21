2e9031e84c0e36187443defb212a219e9c09f3c1
DFVSQY <1520018679@qq.com>
2017-02-21 21:04:48 +0800

    添加使用示例

---
+++ Assets/NoDrawingGraphic/NonDrawingGraphic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+
+public class NonDrawingGraphic : Graphic
+{
+    private NonDrawingGraphic() { useLegacyMeshGeneration = false; }
+
+    public override void SetMaterialDirty() { }
+
+    public override void SetVerticesDirty() { }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        AddClick(gameObject, (go) =>
+        {
+            Debug.Log("OnClick,go:" + go.name);
+        });
+    }
+
+    private void AddClick(GameObject go, UnityAction<GameObject> onClick)
+    {
+        EventTrigger et = go.GetComponent<EventTrigger>();
+        if(et == null)
+        {
+            et = go.AddComponent<EventTrigger>();
+        }
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((data) =>
+        {
+            if(onClick != null) onClick(go);
+
+        });
+        et.triggers.Add(entry);
+    }
+}
